ce169e599107aebb6f83be59df0080d5537ab8fb
davidchoysqldba <davidchoy.sqldba@gmail.com>
2023-09-28 01:09:31 -0400

    Add files via upload

--- fhv_dag.py
+++ fhv_dag.py
@@ -15,24 +15,34 @@ properties = {
 
 def get_json_data(url):
     session = requests.get(url)
-    return session.text
+    return session.json()
+    
 
 
 def get_url_by_last_date_updated(execution_date):
-    return f"https://data.cityofnewyork.us/resource/8wbx-tsch.json?last_date_updated={execution_date}"
+    # return f"https://data.cityofnewyork.us/resource/8wbx-tsch.json?last_date_updated={execution_date}"
+    return "https://data.cityofnewyork.us/resource/8wbx-tsch.json?active=YES"
 
 
 def fetch_data_to_storage(**kwargs):
     execution_date = kwargs["execution_date"]
-    json_data = get_json_data(get_url_by_last_date_updated(str(execution_date)[:10]))
+    # json_data = get_json_data(get_url_by_last_date_updated(str(execution_date)[:10]))
+    json_dict = get_json_data(get_url_by_last_date_updated(str(execution_date)[:10]))
     
+    # json_dict = json.loads(json_data)
+    # json_text = json.dumps(json_dict)
+    json_string = ""
+    for e in json_dict:
+        json_string += "\n" + json.dumps(e)
+
+
     storage_client = storage.Client(project=properties['project_id'])
     bucket_name = "gcp-data-work-storage"
 
     bucket = storage_client.bucket(bucket_name)
     destination_blob_name = f"fhv/dt={str(execution_date)[:10]}/fhv_{str(execution_date)[:10].replace('-', '')}.json"
     blob = bucket.blob(destination_blob_name)
-    blob.upload_from_string(json_data)
+    blob.upload_from_string(json_string[1:])
     
 
 with DAG(
