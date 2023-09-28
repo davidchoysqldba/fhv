0bf2762521d241431e210c49475ba96988c1e31e
davidchoysqldba <davidchoy.sqldba@gmail.com>
2023-09-28 01:11:26 -0400

    Add files via upload

--- bq.sql
+++ bq.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE EXTERNAL TABLE gcp_data_work_ds.fhv
 WITH PARTITION COLUMNS(dt STRING)
 OPTIONS (
-  uris = ['gs://gcp-data-work-storage/fhv/*'],
-  format = 'JSON',
+  uris = ['gs://gcp-data-work-storage/fhv/dt=*'],
+  format = 'NEWLINE_DELIMITED_JSON',
   hive_partition_uri_prefix = 'gs://gcp-data-work-storage/fhv/'
   );
