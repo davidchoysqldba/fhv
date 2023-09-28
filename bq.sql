8aec311cf52fa8bc939cade050425d47664d57b4
davidchoysqldba <davidchoy.sqldba@gmail.com>
2023-09-28 01:52:50 -0400

    Add files via upload

--- bq.sql
+++ bq.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE EXTERNAL TABLE gcp_data_work_ds.fhv
-WITH PARTITION COLUMNS(dt STRING)
+WITH PARTITION COLUMNS(dt DATE)
 OPTIONS (
   uris = ['gs://gcp-data-work-storage/fhv/dt=*'],
   format = 'NEWLINE_DELIMITED_JSON',
